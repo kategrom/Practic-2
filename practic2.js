944ea30ba5e39581f38085bf3a4a0cf97ea99bbb
kategrom <95121842+kategrom@users.noreply.github.com>
2022-04-30 14:48:54 +0300

    Add files via upload

---
+++ practic2.js
@@ -0,0 +1,84 @@
+'use strict'
+
+// Task 1
+
+// const userName = prompt("Ім'я користувача");
+// console.log(userName);
+
+
+// Task 2
+
+// const age1 = 39;
+// const age2 = 18;
+
+// const ageSum = age1 + age2;
+
+// alert(ageSum);
+
+
+// Task 3
+
+// const a = +prompt ("First number");
+// const b = +prompt ("Second number");
+// const sumOfNumber = a*b;
+// console.log(sumOfNumber);
+
+
+// Task 4
+
+// let a1 = 5 + 3;
+// console.log(a1);
+
+// let a2 = 5 - 3;
+// console.log(a2);
+
+// let a3 = 5 * 3;
+// console.log(a3);
+
+// let a4 = 5 / 3;
+// console.log(a4);
+
+
+// Task 5
+
+// let a5 = 5 % 3;
+// alert(a5);
+
+// let a6 = 3 % 5;
+// alert(a6);
+
+// let a7 = 5 + "3";
+// alert(a7);
+
+// let a8 = "5" - 3;
+// alert(a8);
+
+// let a9 = 75 + "кг";
+// alert(a9);
+
+
+// Task 6
+
+// const height = 23;
+// const width = 10;
+
+// const s = height * width;
+// alert(s);
+
+
+// Task 7
+
+// const heightC = 10;
+// const dC = 4;
+
+// const v = Math.PI * (dC / 2) ** 2 * heightC;
+// console.log(v);
+
+
+// Task 8
+
+const n = 3;
+const m = 4;
+
+const k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
+alert(k);
